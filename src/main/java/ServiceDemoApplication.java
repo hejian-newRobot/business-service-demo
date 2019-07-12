5089636eee87089d54210d6e5eb4c19800734794
cutBoy <18207307296@163.com>
2019-07-12 23:34:33 +0800

    spring 组件扫描的基础路径修改

--- src/main/java/ServiceDemoApplication.java
+++ src/main/java/ServiceDemoApplication.java
@@ -2,13 +2,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author hejian
  */
 @EnableFeignClients(basePackages = {"client.*"})
-@SpringBootApplication(scanBasePackages = {"fs.service.business.demoservice", "config"})
+@SpringBootApplication(scanBasePackages = {"fs.service.business.demoservice"})
 @EnableEurekaClient
 public class ServiceDemoApplication {
 
