60377cbf9469e56c5f08920183699582ea771a69
cutBoy <18207307296@163.com>
2019-08-02 01:53:26 +0800

    update business-spring-boot-starter

--- src/main/java/fs/service/business/demoservice/test/ServiceDemoApplication.java
+++ src/main/java/fs/service/business/demoservice/test/ServiceDemoApplication.java
@@ -1,11 +1,15 @@
+package fs.service.business.demoservice.test;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author hejian
  */
+@EnableCircuitBreaker
 @EnableFeignClients(basePackages = {"client.*"})
 @SpringBootApplication(scanBasePackages = {"fs.service.business.demoservice"})
 @EnableEurekaClient
